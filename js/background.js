26a8cc412200d2cd7e69f44e75ba34314689193d
adidottxt <adidottxt@gmail.com>
2020-04-11 18:30:24 -0400

    first commit

---
+++ js/background.js
@@ -0,0 +1,105 @@
+var techno_images = [
+  "https://d2w9rnfcy7mm78.cloudfront.net/3660142/original_095f3a42d8e18344947d68842c4eefde.gif", // whatever
+  "https://d2w9rnfcy7mm78.cloudfront.net/3655603/original_a3a6f55794796a31558ea189793fa91d.gif", // movement
+  "https://d2w9rnfcy7mm78.cloudfront.net/2089709/original_7c9f18a6433601f98a0baff468804ba9.gif", // google maps
+  "https://78.media.tumblr.com/24a228370932b6a51e78863dd712b670/tumblr_nse25brZ6G1sbxdzlo1_500.gif", // poetry
+  "https://d2w9rnfcy7mm78.cloudfront.net/3244045/original_921b3032ab2e66602d432603d79b0197.gif", // code tunnel
+  "https://d2w9rnfcy7mm78.cloudfront.net/2408735/original_8b13d55e059b2f5af2dc44c8b3fbb2ee.gif", // light
+  "https://d2w9rnfcy7mm78.cloudfront.net/3244042/original_a551db7a701f49506b3af847c03fd93e.gif", // globe
+  "https://66.media.tumblr.com/353384ff54cae120a60c054bc727dedf/tumblr_of4dirO18y1u0c1b5o1_540.gif", // white noise
+  "https://d2w9rnfcy7mm78.cloudfront.net/3655640/original_b920ae088b4ad5a294eb1ca603113c22.gif", // face
+  "https://d2w9rnfcy7mm78.cloudfront.net/3660143/original_9d9213ac30f5c29176a479142926734a.gif", // samurai jack
+  "https://d2w9rnfcy7mm78.cloudfront.net/1864782/original_033d8c14ce89231a81a1ea2957401173.gif", // jack nic
+  "https://66.media.tumblr.com/24b200f1f1251fbd7840442d0e5ba6fe/tumblr_pigzajYld51tjkzwpo1_540.gif", // TVs
+  "https://d2w9rnfcy7mm78.cloudfront.net/1735349/original_9e4a83b5edf596cb829e3c513a59e6a8.gif", // diver
+  "https://78.media.tumblr.com/d35608e4289af2238fc7090dbf69a0ba/tumblr_owkzavANcS1u5k06fo1_500.gif", // keep out
+  "https://66.media.tumblr.com/260dc3315dcad5b25be04dc6924892e6/tumblr_my6rwtffm01qg39ewo1_500.gif", // light
+  "https://66.media.tumblr.com/784eede0dd1d1999c607c5ee977c43de/tumblr_n6z471GAbf1t0tqueo1_500.gif", // dude skull
+  "https://66.media.tumblr.com/fb485e1bc9d89f89d755846f85a40557/tumblr_oih27yzyJL1six59bo1_540.gif", // circle dots
+  "https://d2w9rnfcy7mm78.cloudfront.net/549661/original_281370fbfcefc25df35c88f70a76b418.gif", // bridge
+  "https://d2w9rnfcy7mm78.cloudfront.net/2131961/original_10db626677c0d8df06348a51a69aa9f7.gif", // circles
+  "https://d2w9rnfcy7mm78.cloudfront.net/2886080/original_24abc1035d71cf1d1377ed6e51f4e610.gif", // loading
+  "https://d2w9rnfcy7mm78.cloudfront.net/1703752/original_971971ad6791b08ee0dca776f07ced27.gif", // legumes
+  "https://66.media.tumblr.com/0c81d51b719b4a17ede8d7724976bf24/tumblr_nwta35GfoP1tls8aro1_1280.gif", // color white noise
+  "https://66.media.tumblr.com/5197b214a797ce8b37b9fe6026c3cbd9/tumblr_noiwewBBmu1u6x3syo1_r1_540.gif", // dancers
+  "https://d2w9rnfcy7mm78.cloudfront.net/3655604/original_ff61a2acefc8df5f3362511f935bf811.gif", // comet
+  "https://66.media.tumblr.com/16d17bd5c405ee3e352c45de0bf85b85/tumblr_oooul7Fp4L1slaspwo1_500.gif", // skull
+  "https://66.media.tumblr.com/f8511f0bf045986a2c5ef911dae2aaff/tumblr_nfj205fTFr1qe8ju4o1_500.gif", // blocked channel
+  "https://d2w9rnfcy7mm78.cloudfront.net/1719350/original_87148b857ed172bb97dde4099cee5f6d.gif", // sideways rectangle
+  "https://d2w9rnfcy7mm78.cloudfront.net/2125483/original_e6ea3f4596f106ab55ee474f7977abb2.gif", // hands in water
+  "https://78.media.tumblr.com/c4bcb23b7c5eab24e85fa3ef08c816b4/tumblr_mw6dz9Nfho1s8rlddo1_500.gif", // gas masks
+  "https://d2w9rnfcy7mm78.cloudfront.net/2141201/original_503c7b827a0642ec6bf7a492deb9a0ff.gif", // illustration
+  "https://66.media.tumblr.com/bd7f6e59171c1a65109b7daf7af897aa/tumblr_pcjaq5nB9x1w3y4ilo2_500.gif", //moving planets
+  "https://d2w9rnfcy7mm78.cloudfront.net/1953331/original_33f55b289869d524d1fad7b084d4aec5.gif", // blue light
+  "https://66.media.tumblr.com/1ff7858f4d28b33e367f3db97bbbf91b/tumblr_nmciwxQcZ01ql8t12o1_500.gif", // night lights
+  "https://d2w9rnfcy7mm78.cloudfront.net/3666271/original_fef01aec80bf782581be0ac329043727.gif", // color tunnel
+  "https://d2w9rnfcy7mm78.cloudfront.net/3660140/original_cd232121c3bcba6a0a10536cef4d1055.gif", // lines
+  "https://78.media.tumblr.com/6af49747c79dc7a335b085965469e726/tumblr_nnpftf8equ1qd479ro1_640.gif", // emergency lights
+  "https://d2w9rnfcy7mm78.cloudfront.net/3666253/original_98c22e8363cc56199be06656a754e50e.gif", // alex jones
+  "https://d2w9rnfcy7mm78.cloudfront.net/2594794/original_8c6b91e684e54afaec331eca1aa0e294.gif", // dots
+  "https://d2w9rnfcy7mm78.cloudfront.net/1952533/original_78e62570e9d5dcf9d5cc217fef4d675a.gif", // animation page
+  "https://66.media.tumblr.com/50edc9b202f97c3302504b2d5d0fc704/tumblr_oc48fvTO021rwdf4uo1_400.gif", // skull
+  "https://d2w9rnfcy7mm78.cloudfront.net/571930/original_06fe32506233bc6dccb2250c28ce17f0.gif", // smoke bomb kick
+  "https://d2w9rnfcy7mm78.cloudfront.net/2328592/original_2c1bd34a1f79b4de68a89841d643729f.gif", // face
+  "https://d2w9rnfcy7mm78.cloudfront.net/2258564/original_9620f65ee5b0543582afe49bbffde287.gif", // space flow
+  "https://d2w9rnfcy7mm78.cloudfront.net/2394557/original_778954dd6cb23bd580b75f1104b4cdb4.gif", // street
+  "https://d2w9rnfcy7mm78.cloudfront.net/1769511/original_e3bf59229fa7e197da41b6353690640f.gif", // glitch
+  "https://d2w9rnfcy7mm78.cloudfront.net/1864733/original_5aac4af34e2e6619ca34049b0e0d1a57.gif", // running from fire?
+  "https://d2w9rnfcy7mm78.cloudfront.net/3664242/original_c283165aa0510faf0ddc9caa934a0fae.gif", // black dot paint
+  "https://d2w9rnfcy7mm78.cloudfront.net/3655618/original_eb6404f0e455f5ffc239c9ffa8cad73c.gif", // caution
+  "https://d2w9rnfcy7mm78.cloudfront.net/1102404/original_d614d05db1e8ed90bfd0aa87bfb6adbe.gif", // lava
+  "https://d2w9rnfcy7mm78.cloudfront.net/2759492/original_1724532a4717ea109bd85050c1add7f1.gif", // jupiter
+  "https://d2w9rnfcy7mm78.cloudfront.net/2964133/original_4068f0a2d8c5a2dd21c75d8803cb9d2f.gif", // stars
+]
+
+const house_images = [
+  "https://d2w9rnfcy7mm78.cloudfront.net/2818228/original_788e1b4da4c327b0f6879f24cd503832.gif", // color
+  "https://d2w9rnfcy7mm78.cloudfront.net/1891065/original_0f98fd93ad9cd9e0bb61e2fdd14c0591.gif", // color explosion
+  "https://d2w9rnfcy7mm78.cloudfront.net/2109658/original_e24ec2b7498aa5d767a6dc55e1408fbc.gif", // touch me not
+  "https://d2w9rnfcy7mm78.cloudfront.net/1802131/original_0c0b44885d5f27ce53a6be2c89ced08f.gif", // illustration
+  "https://d2w9rnfcy7mm78.cloudfront.net/211208/original_3bc59f9e4b643d988f90093e6dab5512.gif", // color illustration
+  "https://d2w9rnfcy7mm78.cloudfront.net/2305187/original_84dccc2ab0b98a371c3066840239a719.gif", // color collage
+  "https://d2w9rnfcy7mm78.cloudfront.net/2258536/original_15b6737dba7c7f249fc006b53e114b95.gif", // clouds
+  "https://d2w9rnfcy7mm78.cloudfront.net/3655609/original_a7bab9f2cc7f4b89d5c5ea1c71be0f24.gif", // helmet karate
+  "https://d2w9rnfcy7mm78.cloudfront.net/1305198/original_c707e9e3cf895fae36e196b3a2f2af9b.gif", // wavy color
+  "https://66.media.tumblr.com/04538110ef9ee1f668f690249d102eb9/tumblr_nyyarr6oq51uke22ao1_500.gif", // peacock
+  "https://d2w9rnfcy7mm78.cloudfront.net/2141056/original_6a9c4bb43d835c8c54252c09be3f557b.gif", // color dots
+  "https://d2w9rnfcy7mm78.cloudfront.net/1551958/original_9290dc9487c726c1d433f6cbc7316ba7.gif", // love & fear
+  "https://d2w9rnfcy7mm78.cloudfront.net/741291/original_727943b2c3132eb51ff71e4f711c8922.gif", // faces
+  "https://d2w9rnfcy7mm78.cloudfront.net/1104219/original_6a36fd8db97f6c80bbbabc916b47f69e.gif", // painting animation
+  "https://d2w9rnfcy7mm78.cloudfront.net/2252039/original_d125eff113fe83a23c4049e0b91bb00a.gif", // water + ball
+  "https://d2w9rnfcy7mm78.cloudfront.net/1758359/original_9754ceb73371725ad50dd783dc181930.gif", // unimpressed doggo
+  "https://d2w9rnfcy7mm78.cloudfront.net/716386/original_f7f1c70bd4209f83f531f8a3afb98dcf.gif", // dude in room
+  "https://d2w9rnfcy7mm78.cloudfront.net/1269681/original_c7b6f0ec9edc709dec81b36f8e3ae786.gif", // on the edge
+  "https://d2w9rnfcy7mm78.cloudfront.net/741268/original_615162b6bbf564ceeafcd46bbeda5cca.gif", // world illustrated
+  "https://d2w9rnfcy7mm78.cloudfront.net/415465/original_81f80bb8b33fee2f15d9c589230e5466.gif", // slow mo balloon
+  "https://d2w9rnfcy7mm78.cloudfront.net/1907334/original_5f7d1c1ae1155a9274d3731ea29cc61c.gif", // tornado
+  "https://d2w9rnfcy7mm78.cloudfront.net/1910182/original_0f2c9150d941c630aeaa058d10eb9aae.gif", // ice creams
+  "https://d2w9rnfcy7mm78.cloudfront.net/1907320/original_93fbe9a6c6d0faa768a88cc608314517.gif", // bike fall
+  "https://d2w9rnfcy7mm78.cloudfront.net/1917615/original_afe4948a187bce530d4dc49ef2ec4758.gif", // city
+  "https://d2w9rnfcy7mm78.cloudfront.net/1953329/original_4c7bd755539d878f2be9fb0b8b129464.gif", // LA
+  "https://d2w9rnfcy7mm78.cloudfront.net/1931669/original_cc6130abe25c302026d24f44daaa754d.gif", // political party
+  "https://d2w9rnfcy7mm78.cloudfront.net/1809334/original_92e0051406caf05c486e21b78653f05a.gif", // basketball
+  "https://d2w9rnfcy7mm78.cloudfront.net/2141332/original_b172aa41b9d88059009f8bc01babd495.gif", // zuck
+  "https://d2w9rnfcy7mm78.cloudfront.net/2067994/original_651c88a32556ca031e1995262096f60a.gif", // daft punk
+  "https://d2w9rnfcy7mm78.cloudfront.net/2155911/original_bbc11933fda0a70c06456f98a1a41d20.gif", // india
+  "https://d2w9rnfcy7mm78.cloudfront.net/1639434/original_e9de55837ff55d9a54c55e92cc51fd43.gif", // clay cook
+  "https://d2w9rnfcy7mm78.cloudfront.net/1764557/original_2f308bd485b7141b0cd97cff61c8e653.gif", // glitter
+  "https://d2w9rnfcy7mm78.cloudfront.net/1471864/original_9c4fa04225aec1f3159d5f476d536218.gif", // illustrated hand
+  "https://d2w9rnfcy7mm78.cloudfront.net/671830/original_af4a1620cbdbc9d40faa477575fb3fd3.gif", // spinning dude
+  "https://d2w9rnfcy7mm78.cloudfront.net/3670115/original_62c37f15773fbf8f9113910ad2bb33e6.gif", // not the father
+  "https://d2w9rnfcy7mm78.cloudfront.net/3220668/original_d9f32d1468f7db0c10581dc09d745efc.gif", // doggo back
+  "https://d2w9rnfcy7mm78.cloudfront.net/110653/original_f3793e74b49815a6445aa7998d22197c.gif", // squirtles
+]
+
+const imageHead = document.getElementById("full-background");
+
+let i = 0;
+
+setInterval(function() {
+      imageHead.style.backgroundImage = "url(" + techno_images[i] + ")";
+      i = i + 1;
+      if (i == images.length) {
+      	i =  0;
+      }
+}, 3000);
